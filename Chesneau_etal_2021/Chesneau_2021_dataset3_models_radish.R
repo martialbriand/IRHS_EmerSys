abac84156d58ff9efde980612537618826b5e5b8
Martial Briand <martial.briand@inra.fr>
2021-05-26 14:26:20 +0200

    Add datas and scripts Chesneau et al 2021 "Single seed microbiota : dynamics and transmission from mother plant to seedling"

---
+++ Chesneau_etal_2021/Chesneau_2021_dataset3_models_radish.R
@@ -0,0 +1,462 @@
+
+#title: "Single seed microbiota: assembly and transmission from parent plant to seedling"
+#author: "Chesneau et al., 2021"
+#date: "21/05/2021"
+
+setwd("~/These2/Script R/F2S-diversite/F2S-year-4/Markdown/Dossier_run_script_vf/Analyse_beatrice/")
+library(stats)
+library(data.table)
+library(MASS)
+library(bvls)
+library(BiocManager)
+library(TailRank)
+library(DescTools)
+plot.new()
+
+################################
+# USER SUPPLIED INFORMATIONS
+# abundance file, total number of plants in the experiment
+ficname="otu_table_16Sr.csv"
+sep=','
+Nplanttot=15
+# to simulate the neutral model: we need the number of draws in the distribution of N (number of simulated seeds) and
+# a grid for the model parameters S and R
+Ndraw=1000
+S=2:100
+R= (1:250)*0.000001#mu/((1-mu)*Cstar)
+#data filtering
+type=1 #1:remove individual OTUs whose proportion within one seed is less than threshold (0.3% works, 0.5% works fine)
+#2: remove all OTUs whose cumulated proportions within one seed is less than threshold (1% works fine)
+threshold=1 #threshold for data filtering in % 
+##################################
+
+#read data files, samples are lines, ASV are columns
+ASVrad<-read.csv(file = ficname,sep=sep,header=TRUE,)
+day=unique(ASVrad$DAP)
+
+
+
+dayplant=NULL
+Fday=list()
+M1list=list()
+M2list=list()
+M3list=list()
+nl=0
+lambda=NULL
+sd=NULL
+res=NULL
+Filtering=NULL
+
+indexASV=which(substr(colnames(ASVrad),1,3)=="ASV")
+C=ASVrad[,indexASV]
+
+# how does the distribution of the number of ASV detected in seeds look like?
+#the answer is: bof...
+par(mfrow=c(1,1))
+Ntot=rowSums(C)
+qu01=quantile(Ntot,probs=c(0.,1))
+x=runif(2000, min=qu01[1], max=qu01[2])
+#qqplot(x,Ntot)
+#qqline(Ntot, distribution = function(p) qunif(p, min=min(Ntot), max=max(Ntot)), probs = c(0., 1), col = 2)
+#just to know ...but not so bad on dates separately, except
+
+
+#loop on dates
+for (j in day) {
+indexDAP=which(ASVrad$DAP==j)
+ASVradj=ASVrad[indexDAP,]
+Ntotj=Ntot[indexDAP]
+# check if uniform distribution is acceptable (uncomment if necessary)
+#qu01=quantile(Ntotj,probs=c(0.,1))
+#x=runif(2000, min=qu01[1], max=qu01[2])
+#qqplot(x,Ntotj)
+#qqline(Ntotj, distribution = function(p) qunif(p, min=min(Ntotj), max=max(Ntotj)), probs = c(0., 1), col = 2)
+
+# and now compute 0.1-0.9 quantiles and store them
+qu=quantile(Ntotj,probs=c(0.1,0.9))
+lambda=rbind(lambda, qu)
+#
+#now loop on plants within each day
+plant=unique(ASVradj$Plant)
+  for (p in plant){
+    indexP=which(ASVradj$Plant==p)
+    ASVradjp=ASVradj[indexP,]
+    nseed=dim(ASVradjp)[1]
+# number of seeds per plant and date
+    A=transpose(ASVradjp[,indexASV])
+    row.names(A)=colnames(ASVradjp[indexASV])
+    index=which(rowSums(A)>0)
+    Splant=sum(index>0)
+    data0=sum(A>0)
+# number of ASV present at least once per time and date
+    A=A[index,]
+    L=sort(A[,1], decreasing = TRUE,index.return=TRUE)
+    B=as.vector(L$x)
+    if (type==2){
+      Nthresh=floor(sum(B)*(1-threshold/100))
+      filter=which(cumsum(B)>Nthresh)
+      B[filter[2:length(filter)]]=0 }
+    else {
+      Nthresh=floor(sum(B)*threshold/100)
+      filter=which(B<=Nthresh)
+      B[filter]=0 }
+    #here we need sorting
+    F=B/sum(B) #frequency per seed
+    for (i in (2:nseed)){
+      l=sort(A[,i], decreasing = TRUE, index.return=TRUE)
+      b=as.vector(l$x)
+      if (type==2){
+        Nthresh=floor(sum(b)*(1-threshold/100))
+        filter=which(cumsum(b)>Nthresh)
+        b[filter[2:length(filter)]]=0}
+      else {
+        Nthresh=floor(sum(b)*threshold/100)
+        filter=which(b<=Nthresh)
+        b[filter]=0 }
+      B=cbind(B,b)
+      F=cbind(F,b/sum(b))
+    }
+  ## now compute and store first three moment on the non zero asv
+  m1Fpos=0*(1:dim(F)[2])
+  m2Fpos=0*(1:dim(F)[2])
+  m3Fpos=0*(1:dim(F)[2])
+  for (s in 1:dim(F)[2]){
+    m1Fpos[s]=mean(F[which(F[,s]>0),s])
+    m2Fpos[s]=mean(F[which(F[,s]>0),s]^2)
+    m3Fpos[s]=mean(F[which(F[,s]>0),s]^3)
+  }
+  datafiltered=sum(B>0)
+  Filtering=c(Filtering, datafiltered/data0)
+  nl=nl+1
+  Fday[[nl]]=F
+  M1list[[nl]]=m1Fpos
+  M2list[[nl]]=m2Fpos
+  M3list[[nl]]=m3Fpos
+  daynumber=which(day==j)
+  plantnumber=which(plant==p)
+  dayplant[[nl]]=c(daynumber,plantnumber)
+}
+}
+#impact of filtering: proportion of non zero data kept after filtering for each plant
+hist(Filtering)
+
+# moment estimation of kappa, S and R related to neutral model
+popt=NULL
+popt2=NULL
+popt3=NULL
+#
+for (nl in 1:Nplanttot){
+p=dayplant[[nl]]
+set.seed(nl)
+Nsample=sample(lambda[p[1],1]:lambda[p[1],2], Ndraw, replace=T)
+# draw values for N in the suitable uniform distribution 
+err=NULL
+err2=NULL
+err3=NULL
+param=NULL 
+count=0
+M1obs=mean(M1list[[nl]])
+M2obs=mean(M2list[[nl]])
+M3obs=mean(M3list[[nl]])
+for (s in S) {
+  for (r in R) {
+count=count+1
+Ic=r*(Nsample-1)
+alphac=Ic/s
+betac=Ic-alphac
+#
+P0=dbb(0,Nsample,alphac,betac)
+Em1=mean(1/(s*(1-P0)))
+errM1=abs(Em1-M1obs)
+numerator=1+alphac+betac/Nsample
+denominator1=(1+Ic)*s*(1-P0)
+Em2=mean(numerator/denominator1)
+errM2=abs(Em2-M2obs)
+numerator=(1+alphac)*((2+alphac)+3*betac/Nsample)+betac*(betac-alphac)/(Nsample^2)
+denominator2=denominator1*(2+Ic)
+Em3=mean(numerator/denominator2)
+errM3=abs(Em3-M3obs)
+param[[count]]=c(s,r)
+err3=c(err3, sqrt(errM1^2+(errM2/M1obs)^2+(errM3/M2obs)^2))
+err2=c(err2, sqrt((errM1/M1obs)^2+(errM2/M2obs)^2+(errM3/M3obs)^2))
+err=c(err, sqrt(errM1^2+(errM2)^2+(errM3)^2))
+   }
+}
+bestp=param[[which(err==min(err))]]
+ic=bestp[2]*(mean(Nsample)-1) #R*(expected (N)-1)
+alpha=ic/bestp[1]
+popt=rbind(popt,c(p,param[[which(err==min(err))]], ic, alpha, err[which(err==min(err))]))
+bestp=param[[which(err2==min(err2))]]
+ic=bestp[2]*(mean(Nsample)-1) #R*(expected (N)-1)
+alpha=ic/bestp[1]
+popt2=rbind(popt2,c(p,param[[which(err2==min(err2))]], ic, alpha, err2[which(err2==min(err2))]))
+bestp=param[[which(err3==min(err3))]]
+ic=bestp[2]*(mean(Nsample)-1) #R*(expected (N)-1)
+alpha=ic/bestp[1]
+popt3=rbind(popt3,c(p,param[[which(err3==min(err3))]], ic, alpha, err3[which(err3==min(err3))]))
+}
+
+
+
+par(mfrow=c(1,2))
+boxplot(popt[,7]~popt[,1],outline=FALSE,names = day, main = "Error")
+boxplot(popt[,3]~popt[,1],outline=FALSE,names = day, main = "S")
+par(mfrow=c(1,2))
+boxplot(popt[,5]~popt[,1],outline=FALSE,names =day, main = "ratio Iplant")
+boxplot(popt[,4]~popt[,1],outline=FALSE,names = day, main = "R")
+
+wilcox.test(popt[1:10,5],popt[11:15,5],alternative="less")
+wilcox.test(popt[5:10,5],popt[11:15,5],alternative="less")
+wilcox.test(popt[1:5,5],popt[6:10,5],alternative="greater")
+
+
+
+#Write file
+write.csv(popt, "popt_r_16S")
+
+#Prepare data plot
+poptf <- data.frame(popt)
+colnames(poptf) <- c("DAP", "X2", "S", "R", "ratio_Iplant", "X6", "Error")
+poptf$DAP<-ifelse(poptf$DAP == 1,'D26',
+                  ifelse(poptf$DAP == 2,'D37',
+                                ifelse(poptf$DAP == 4,'D65','D65')))
+
+#Prepare palette color for habitats
+name_DAP <- c("D02","D27", "D33", "D42", "D50", "D03","D26", "D37", "D65")
+
+# desired color palette
+DAPPalette <- c( "#F0FEEF","#F5F6CE", "#D0F5A9", "#5FB404", "#0B610B","#FFF8EC", "#F6E3CE", "#FAAC58", "#DF3A01")
+
+# associated EnvType level
+names(DAPPalette) <- name_DAP
+
+#Write file
+write.csv(poptf, "poptf_r_16S")
+
+#Open data
+poptf_r_16S <- read.csv("poptf_r_16S", sep = ",",  check.names=FALSE, row.names=1)
+poptf_r_gyrB <- read.csv("poptf_r_gyrB", sep = ",",  check.names=FALSE, row.names=1)
+
+poptf_r_16S$Plant.species <- c("Radish")
+poptf_r_gyrB$Plant.species <- c("Radish")
+
+#Plot
+
+library(ggplot2)
+theme_set(theme_rw())
+
+Plot_16Sr_Iplant <- ggplot(data=poptf_r_16S, aes_string(x='DAP',y='ratio_Iplant', fill='DAP')) + 
+  geom_boxplot(outlier.shape = NA) + 
+  geom_jitter(aes(color=DAP)) +
+  theme(strip.background = element_rect(color="white", fill="#585858", size=1, linetype="solid"),
+        strip.text.x = element_text(size = 12, color = "white"),
+        axis.title.x = element_text( size = 12, face = "bold"),
+        axis.title.y = element_text( size = 12, face = "bold"),
+        plot.title = element_text(size = 16),
+        axis.text.x = element_text(), 
+        axis.text.y = element_text())+ 
+  ylab ("Ratio I plant") + xlab ("Day after polination") +
+  facet_grid(cols = vars(Plant.species), scales = "free") +
+  ggtitle("Ratio I plant - 16S") + 
+  scale_fill_manual(values=DAPPalette) + 
+  scale_color_manual(values=DAPPalette)
+Plot_16Sr_Iplant
+
+Plot_16Sr_R <- ggplot(data=poptf_r_16S, aes_string(x='DAP',y='R', fill='DAP')) + 
+  geom_boxplot(outlier.shape = NA) + 
+  geom_jitter(aes(color=DAP)) +
+  theme(strip.background = element_rect(color="white", fill="#585858", size=1, linetype="solid"),
+        strip.text.x = element_text(size = 12, color = "white"),
+        axis.title.x = element_text( size = 12, face = "bold"),
+        axis.title.y = element_text( size = 12, face = "bold"),
+        plot.title = element_text(size = 16),
+        axis.text.x = element_text(), 
+        axis.text.y = element_text())+ 
+  ylab ("R") + xlab ("Day after polination") +
+  facet_grid(cols = vars(Plant.species), scales = "free") +
+  ggtitle("R - 16S") + 
+  scale_fill_manual(values=DAPPalette) + 
+  scale_color_manual(values=DAPPalette)
+Plot_16Sr_R
+
+Plot_16Sr_Error <- ggplot(data=poptf_r_16S, aes_string(x='DAP',y='Error', fill='DAP')) + 
+  geom_boxplot(outlier.shape = NA) + 
+  geom_jitter(aes(color=DAP)) +
+  theme(strip.background = element_rect(color="white", fill="#585858", size=1, linetype="solid"),
+        strip.text.x = element_text(size = 12, color = "white"),
+        axis.title.x = element_text( size = 12, face = "bold"),
+        axis.title.y = element_text( size = 12, face = "bold"),
+        plot.title = element_text(size = 16),
+        axis.text.x = element_text(), 
+        axis.text.y = element_text())+ 
+  ylab ("Error") + xlab ("Day after polination") +
+  facet_grid(cols = vars(Plant.species), scales = "free") +
+  ggtitle("Error - 16S") + 
+  scale_fill_manual(values=DAPPalette) + 
+  scale_color_manual(values=DAPPalette)
+Plot_16Sr_Error
+
+Plot_16Sr_S <- ggplot(data=poptf_r_16S, aes_string(x='DAP',y='S', fill='DAP')) + 
+  geom_boxplot(outlier.shape = NA) + 
+  geom_jitter(aes(color=DAP)) +
+  theme(strip.background = element_rect(color="white", fill="#585858", size=1, linetype="solid"),
+        strip.text.x = element_text(size = 12, color = "white"),
+        axis.title.x = element_text( size = 12, face = "bold"),
+        axis.title.y = element_text( size = 12, face = "bold"),
+        plot.title = element_text(size = 16),
+        axis.text.x = element_text(), 
+        axis.text.y = element_text())+ 
+  ylab ("S") + xlab ("Day after polination") +
+  facet_grid(cols = vars(Plant.species), scales = "free") +
+  ggtitle("S - 16S") + 
+  scale_fill_manual(values=DAPPalette) + 
+  scale_color_manual(values=DAPPalette)
+Plot_16Sr_S
+
+
+library(cowplot); packageVersion('cowplot')
+Plot_16S_r <- plot_grid(Plot_16Sr_Iplant, Plot_16Sr_R, Plot_16Sr_Error, Plot_16Sr_S)
+
+
+
+Plot_gyrBr_Iplant <- ggplot(data=poptf_r_gyrB, aes_string(x='DAP',y='ratio_Iplant', fill='DAP')) + 
+  geom_boxplot(outlier.shape = NA) + 
+  geom_jitter(aes(color=DAP)) +
+  theme(strip.background = element_rect(color="white", fill="#585858", size=1, linetype="solid"),
+        strip.text.x = element_text(size = 12, color = "white"),
+        axis.title.x = element_text( size = 12, face = "bold"),
+        axis.title.y = element_text( size = 12, face = "bold"),
+        plot.title = element_text(size = 16),
+        axis.text.x = element_text(), 
+        axis.text.y = element_text())+ 
+  ylab ("Ratio I plant") + xlab ("Day after polination") +
+  facet_grid(cols = vars(Plant.species), scales = "free") +
+  ggtitle("Ratio I plant - gyrB") + 
+  scale_fill_manual(values=DAPPalette) + 
+  scale_color_manual(values=DAPPalette)
+Plot_gyrBr_Iplant
+
+Plot_gyrBr_R <- ggplot(data=poptf_r_gyrB, aes_string(x='DAP',y='R', fill='DAP')) + 
+  geom_boxplot(outlier.shape = NA) + 
+  geom_jitter(aes(color=DAP)) +
+  theme(strip.background = element_rect(color="white", fill="#585858", size=1, linetype="solid"),
+        strip.text.x = element_text(size = 12, color = "white"),
+        axis.title.x = element_text( size = 12, face = "bold"),
+        axis.title.y = element_text( size = 12, face = "bold"),
+        plot.title = element_text(size = 16),
+        axis.text.x = element_text(), 
+        axis.text.y = element_text())+ 
+  ylab ("R") + xlab ("Day after polination") +
+  facet_grid(cols = vars(Plant.species), scales = "free") +
+  ggtitle("R - gyrB") + 
+  scale_fill_manual(values=DAPPalette) + 
+  scale_color_manual(values=DAPPalette)
+Plot_gyrBr_R
+
+Plot_gyrBr_Error <- ggplot(data=poptf_r_gyrB, aes_string(x='DAP',y='Error', fill='DAP')) + 
+  geom_boxplot(outlier.shape = NA) + 
+  geom_jitter(aes(color=DAP)) +
+  theme(strip.background = element_rect(color="white", fill="#585858", size=1, linetype="solid"),
+        strip.text.x = element_text(size = 12, color = "white"),
+        axis.title.x = element_text( size = 12, face = "bold"),
+        axis.title.y = element_text( size = 12, face = "bold"),
+        plot.title = element_text(size = 16),
+        axis.text.x = element_text(), 
+        axis.text.y = element_text())+ 
+  ylab ("Error") + xlab ("Day after polination") +
+  facet_grid(cols = vars(Plant.species), scales = "free") +
+  ggtitle("Error - gyrB") + 
+  scale_fill_manual(values=DAPPalette) + 
+  scale_color_manual(values=DAPPalette)
+Plot_gyrBr_Error
+
+Plot_gyrBr_S <- ggplot(data=poptf_r_gyrB, aes_string(x='DAP',y='S', fill='DAP')) + 
+  geom_boxplot(outlier.shape = NA) + 
+  geom_jitter(aes(color=DAP)) +
+  theme(strip.background = element_rect(color="white", fill="#585858", size=1, linetype="solid"),
+        strip.text.x = element_text(size = 12, color = "white"),
+        axis.title.x = element_text( size = 12, face = "bold"),
+        axis.title.y = element_text( size = 12, face = "bold"),
+        plot.title = element_text(size = 16),
+        axis.text.x = element_text(), 
+        axis.text.y = element_text())+ 
+  ylab ("S") + xlab ("Day after polination") +
+  facet_grid(cols = vars(Plant.species), scales = "free") +
+  ggtitle("S - gyrB") + 
+  scale_fill_manual(values=DAPPalette) + 
+  scale_color_manual(values=DAPPalette)
+Plot_gyrBr_S
+
+
+library(cowplot); packageVersion('cowplot')
+Plot_gyrB_r <- plot_grid(Plot_gyrBr_Iplant, Plot_gyrBr_R, Plot_gyrBr_Error, Plot_gyrBr_S)
+
+
+#Stats
+pairwise.wilcox.test(poptf_r_16S[,5], poptf_r_16S[,1], p.adjust.method="BH")
+# cool 
+pairwise.wilcox.test(poptf_r_16S[,7], poptf_r_16S[,1], p.adjust.method="BH")
+# cool
+
+pairwise.wilcox.test(poptf_r_16S[,3], poptf_r_16S[,1], p.adjust.method="BH")
+#
+wilcox.test(poptf_r_16S[6:10,3],poptf_r_16S[11:15,3],alternative="less")
+
+
+pairwise.wilcox.test(poptf_r_gyrB[,5], poptf_r_gyrB[,1], p.adjust.method="BH")
+# cool 
+pairwise.wilcox.test(poptf_r_gyrB[,6], poptf_r_gyrB[,1], p.adjust.method="BH")
+# cool
+wilcox.test(poptf_r_gyrB[1:10,7],poptf_r_gyrB[11:15,7],alternative="greater")
+
+pairwise.wilcox.test(poptf_r_gyrB[,3], poptf_r_gyrB[,1], p.adjust.method="BH")
+#
+wilcox.test(poptf_r_gyrB[6:10,3],poptf_r_gyrB[11:15,3],alternative="less")
+
+
+wilcox.test(poptf_r_gyrB[6:10,5],poptf_r_gyrB[11:15,5],alternative="less")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+par(mfrow=c(2,4))
+Ndraw=500
+for (nl in 1:Nplanttot){
+  PD=as.vector(Fday[[nl]])
+  titre=paste("Day",day[popt[nl,1]],"Plant",popt[nl,2],sep=" ")
+  hist(PD[which(PD>0)], (0:100)/100,freq=FALSE,main=titre,xlab="proportion",ylab="Probability density")
+
+  S=popt[nl,3]
+  R=popt[nl,4]
+  set.seed(nl)
+  Nsample=sample(lambda[popt[nl,1],1]:lambda[popt[nl,1],2], Ndraw, replace=T)
+  Nmax=max(Nsample)
+  Fn=NULL
+  Pn=NULL
+  for (N in Nsample) {
+    IN=R*(N-1)
+    alphaN=IN/S
+    betaN=IN-alphaN
+    #
+    P0=dbb(0,N,alphaN,betaN)
+    Pn=append(Pn,dbb(1:N,N,alphaN,betaN)/(1-P0),after=length(Pn))
+    Fn=append(Fn,(1:N)/N,after=length(Fn))
+  }
+  L=sort(Fn,index.return=TRUE)
+  Pntrie=Pn[L$ix]
+  Dens=0*(1:100)
+  for (i in 1:100) {Dens[i]=sum(Pntrie[which(L$x>(i-1)/100 & L$x<=i/100)])/Ndraw}
+  lines((1:100)/100, 100*Dens,col="red")
+}
+par(mfrow=c(1,1))
